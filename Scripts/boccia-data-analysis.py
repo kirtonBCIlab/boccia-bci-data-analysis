1afd2a418864441631e6f5208c5f29b07b86117e
daniellarbourque <daniellarosebourque@gmail.com>
2025-04-23 14:21:36 -0600

    Add comments and clean up

--- Scripts/boccia-data-analysis.py
+++ Scripts/boccia-data-analysis.py
@@ -17,15 +17,15 @@ class BocciaDataAnalysis:
         stream_name : str
             Name of the target element stream (if Play or VirtualPlay was used)  
         """
-        self.folder_path = folder_path
-        self.eeg_files = None
-        self.trial_settings_files = None
-        self.trial_data = self.get_trial_data()
+        self.folder_path = folder_path # Path to the folder containing the data
+        self.eeg_files = None # Stores EEG data files
+        self.trial_settings_files = None # Stores trial settings files
 
-        self.print_confusion_matrix = True
-        self.trial_count = 0
+        self.print_confusion_matrix = True # Flag to print confusion matrix
+        self.trial_count = 0 # Counter for the number of processed trials
 
-        self.target_stream_name = stream_name
+        # Names of the relevant streams
+        self.target_stream_name = stream_name 
         self.python_response_stream_name = "PythonResponse"
 
         # Initialize target element stream variables
@@ -36,10 +36,15 @@ class BocciaDataAnalysis:
         self.python_response_markers = None
         self.python_response_time = None
 
+        # Initialize trial ID dictionary
         self.trial_ID_dict = self.initialize_trial_dict()
 
+        # Initialize prediction accuracy and trial ID lists
         self.prediction_accuracies = []
         self.trial_IDs = []
+        
+        # Get trial data to process and analyze
+        self.trial_data = self.get_trial_data()
 
     def initialize_trial_dict(self):
         """
@@ -80,9 +85,11 @@ class BocciaDataAnalysis:
         trial_data : dict
             Dictionary mapping trial numbers to trial data 
         """
+        # Call the methods to retrieve EEG data and trial settings
         self.eeg_files = self.retrieve_eeg_files()
         self.trial_settings_files = self.retrieve_trial_settings_files()
         
+        # Pair EEG data and trial settings based on trial number
         trial_data = {}
         for trial_number in sorted(set(self.eeg_files.keys()) & set(self.trial_settings_files.keys())):
             trial_data[trial_number] = {
@@ -105,6 +112,7 @@ class BocciaDataAnalysis:
         eeg_files : dict
             Dictionary mapping trial numbers to EEG data file paths
         """
+        # Locate EEG data files from the folder using regular expression
         eeg_files = {}
         with os.scandir(self.folder_path) as entries:
             for entry in entries:
@@ -128,6 +136,7 @@ class BocciaDataAnalysis:
         trial_settings : dict
             Dictionary mapping trial numbers to trial settings file paths
         """
+        # Locate trial settings files from the folder using regular expression
         trial_settings = {}
         with os.scandir(self.folder_path) as entries:
             for entry in entries:
@@ -153,9 +162,11 @@ class BocciaDataAnalysis:
         -------
         None
         """
+        # Process the EEG data
         xdf_file = self.trial_data[trial_number]["eeg_data"]
         self.process_streams(xdf_file)
 
+        # Process the trial settings
         settings_file = self.trial_data[trial_number]["trial_settings"]
         trial_ID = self.process_settings(settings_file)
         self.trial_IDs.append(trial_ID)
@@ -176,9 +187,8 @@ class BocciaDataAnalysis:
         # Load the xdf file
         streams, fileheader = pyxdf.load_xdf(xdf_file)
 
-        # Get data from streams of interest
+        # Get data from streams of interest (target stream and python response stream)
         self.target_markers, self.target_time = self.get_stream_data(streams, self.target_stream_name)
-        # print(self.target_markers)
         self.python_response_markers, self.python_response_time = self.get_stream_data(streams, self.python_response_stream_name)
 
         # Get list of actual target elements
@@ -208,10 +218,12 @@ class BocciaDataAnalysis:
         stream_time : list
             List of timestamps from the stream
         """
+        # Find the index of the stream based on its name
         stream_index = next((i for i, stream in enumerate(streams) if stream_name in stream['info']['name'][0]), None)
-        # print(f"Stream {stream_index}: {streams[stream_index]['info']['name'][0]}")
+        # Set the stream of interest
         stream = streams[stream_index]
 
+        # Get markers and timestamps from the stream
         stream_markers = stream['time_series']
         stream_time = stream['time_stamps']
 
@@ -231,6 +243,7 @@ class BocciaDataAnalysis:
         target_elements : list
             List of target numbers extracted from the stream
         """
+        # Extract a list of target element numbers
         target_elements = [int(item[1].split(": ")[1]) for item in target_markers]
         return target_elements
     
@@ -248,13 +261,14 @@ class BocciaDataAnalysis:
         predicted_targets : list
             List of predicted target numbers extracted from the stream
         """
-        # Extract predictions from Python response stream by removing 'ping' and 'marker received' messages
+        # Isolate predictions from Python response stream by removing 'ping' and 'marker received' messages
         extracted_predictions = [
             s for sublist in python_response_markers for s in sublist
             if 'ping' not in str(s).lower() and 'marker received' not in str(s).lower()
         ]
 
-        predicted_targets = [int(s.strip('[]')) for s in extracted_predictions] # Format as list of integers
+        # Convert the list of strings to a list of integers
+        predicted_targets = [int(s.strip('[]')) for s in extracted_predictions]
         return predicted_targets
 
     def compare_predictions(self, predicted_targets, target_element_iSPOs):
@@ -273,10 +287,10 @@ class BocciaDataAnalysis:
         -------
         None
         """
-        # Make sure number of predictions matches number of actual targets
+        # Check that the number of predictions matches number of actual targets
         assert len(predicted_targets) == len(target_element_iSPOs)
 
-        # Convert to binary classification: 1 if match, 0 if mismatch
+        # Convert to binary classification: 1 if the target matches, 0 if it doesn't match
         binary_preds = [1 if pred == true else 0 for pred, true in zip(predicted_targets, target_element_iSPOs)]
         binary_truths = [1] * len(target_element_iSPOs)  # All actual targets are correct
 
@@ -284,12 +298,14 @@ class BocciaDataAnalysis:
         cm = confusion_matrix(binary_truths, binary_preds, labels=[1, 0])
         accuracy = accuracy_score(binary_truths, binary_preds) * 100
 
+        # Print confusion matrix and accuracy if print_confusion_matrix is True
         if self.print_confusion_matrix:
             self.trial_count += 1
             print(f"Trial {self.trial_count} confusion matrix:")
             print(cm)
             print(f"Accuracy: {accuracy:.2f}%\n")
 
+        # Add the accuracy (percentage of correct predictions) to the list
         self.prediction_accuracies.append(accuracy)
 
     def process_settings(self, settings_file):
@@ -315,7 +331,10 @@ class BocciaDataAnalysis:
             stim_type = settings["P300Settings"]["Test"]["StimulusType"]
             separate_buttons = settings["P300Settings"]["SeparateButtons"]
 
+        # Determine the trial ID based on the settings values
         trial_ID = None
+
+        # Check stimulus type and return if it was "Face Sprite" or "Gradient"
         if stim_type == 1:
             trial_ID = 4
             return trial_ID
@@ -323,10 +342,12 @@ class BocciaDataAnalysis:
             trial_ID = 5
             return trial_ID
         
+        # Check if separate buttons were used, return if so
         if separate_buttons:
             trial_ID = 6
             return trial_ID
         
+        # Finally, check the number of columns
         if num_cols == 7:
             trial_ID = 1
         elif num_cols == 3:
@@ -334,6 +355,7 @@ class BocciaDataAnalysis:
         elif num_cols == 5:
             trial_ID = 3
 
+        # If none of the above conditions were met, return None
         else:
             print("Trial ID could not be determined.")
 
@@ -351,26 +373,37 @@ class BocciaDataAnalysis:
         -------
         None
         """
+        # Print accuracy (percentage of correct predictions) for each trial
         print("Prediction Accuracies:")
         for i in range(len(self.prediction_accuracies)):
             print(f"{self.prediction_accuracies[i]:.2f}%")
 
+        # Print trial IDs for each trial
         print("\nTrial IDs:")
         for i in range(len(self.trial_IDs)):
             print(self.trial_IDs[i])
 
+        # Print trial descriptions using the trial ID dictionary
         print("\nTrial Descriptions:")
         for i in range(len(self.trial_IDs)):
             print(self.trial_ID_dict[self.trial_IDs[i]])
 
 def main():
-    folder_path = "D:/Daniella Bourque/Boccia Validation/Participant-Data/250423_Participant_1_Data_Electrode" # Path to the folder containing the data
+    # Path to the folder containing the data to analyze
+    folder_path = "D:/Daniella Bourque/Boccia Validation/Participant-Data/250423_Participant_1_Data_Electrode"
+
+    # Name of the target element stream: "TargetElementStream_Play" or "TargetElementStream_VirtualPlay"
+    # Depending on whether Play Boccia or Virtual Play mode was used when collecting the data
     target_stream_name = "TargetElementStream_VirtualPlay"
+
+    # Initialize the BocciaDataAnalysis class
     boccia_data_analysis = BocciaDataAnalysis(folder_path, target_stream_name)
 
+    # Process the trials
     for trial_number in boccia_data_analysis.trial_data:
         boccia_data_analysis.process_trial(trial_number)
 
+    # Output the results
     boccia_data_analysis.print_results()
 
 if __name__ == "__main__":
